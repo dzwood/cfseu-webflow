cfe1fdda50dffd85e737166405c217be6ffbcc8d
Daniel Wood <Dzwood327@gmail.com>
2024-06-21 16:33:07 -0400

    Suppressed hyrdation warnings for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,10 @@ import { getWebflowData } from "./getWebflow";
 export default async function Home() {
   const { props } = await getWebflowData(null);
 
-  return <div dangerouslySetInnerHTML={{ __html: props?.bodyContent }} />;
+  return (
+    <div
+      suppressHydrationWarning={true}
+      dangerouslySetInnerHTML={{ __html: props?.bodyContent }}
+    />
+  );
 }
